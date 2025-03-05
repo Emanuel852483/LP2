666a350a748d09b0f249f95e74b0f91809935539
Gisele Branco <1231763@isep.ipp.pt>
2025-03-05 22:50:30 +0000

    primeiro commit

--- Leilao/src/Model/Estatisticas.java
+++ Leilao/src/Model/Estatisticas.java
@@ -1,4 +1,10 @@
 package Model;
+package Controller;
+
+import Controller.ClienteController;
 
 public class Estatisticas {
+    public Estatisticas(ClienteController clienteController) {
+
+    }
 }
