1d9e9b805e387d927ae3ba8382359148426e98a5
Emanuel Maia <1231531@isep.ipp.pt>
2025-04-15 21:24:21 +0100

    primeiro commit

--- Leilao/src/Main.java
+++ Leilao/src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Controller.NotificacaoController;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        NotificacaoController notificacaoController = new NotificacaoController();
+
+        System.out.println("===== FORMULÁRIO DE REGISTO =====");
+
+        System.out.print("Nome: ");
+        String nomeDoUtilizador = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String emailDoUtilizador = scanner.nextLine();
+
+        // Verificação básica do formato do e-mail
+        if (!emailDoUtilizador.matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
+            System.out.println("Email inválido! Por favor, insira um e-mail válido.");
+        } else {
+            // Tenta enviar o e-mail
+            boolean enviado = notificacaoController.enviarEmail(emailDoUtilizador, nomeDoUtilizador);
+
+            if (enviado) {
+                System.out.println("Registo concluído. Verifique o seu e-mail para a mensagem de boas-vindas!");
+            } else {
+                System.out.println("Ocorreu um erro ao enviar o e-mail. Tente novamente mais tarde.");
+            }
         }
+
+        scanner.close();
     }
 }
