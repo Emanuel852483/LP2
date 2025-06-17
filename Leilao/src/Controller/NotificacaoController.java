e6ade87af6faa5355d258c03116804ba3f0097c2
1241597 <1241597@isep.ipp.pt>
2025-06-17 22:52:55 +0100

    commit simao

--- Leilao/src/Controller/NotificacaoController.java
+++ Leilao/src/Controller/NotificacaoController.java
@@ -1,8 +1,8 @@
 package Controller;
 
 import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.*;
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
 
 public class NotificacaoController {
 
